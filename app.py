2d7fb4cf305c19c5f8bd7a6d6cd30b804919460d
Dipanshu Ashok Agarwal <hu1243009@sjchs.edu.in>
2025-07-14 18:51:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from flask import render_template
 app = Flask(__name__)
 CORS(app)  # allows frontend to call this API
 
-
 @app.route("/")
 def get_message():
+    mylist = []
+    for x in range(100):
+        mylist.append(f"Index {x}")
     return render_template("index.html")
