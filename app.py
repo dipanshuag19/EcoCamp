231d3769161801262e266d9fbf963cc4c2e9ea8e
Dipanshu Ashok Agarwal <hu1243009@sjchs.edu.in>
2025-07-14 18:40:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=".")
 CORS(app)  # allows frontend to call this API
 
-@app.route("/api/message")
+
+@app.route("/")
 def get_message():
-    return jsonify({"message": "Hello from Flask on Koyeb!"})
+    return render_template("index.html")
