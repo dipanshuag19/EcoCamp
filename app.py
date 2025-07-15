32042560fa91ac8a00b84a530b9b2e8aab7c98c3
Dipanshu Ashok Agarwal <hu1243009@sjchs.edu.in>
2025-07-15 19:36:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,35 +7,44 @@ app = Flask(__name__)
 db = sq.connect(os.environ.get("SQLITECLOUD"))
 c = db.cursor()
 
-# Create table
-c.execute("CREATE TABLE IF NOT EXISTS hi(id INT primary key, name VARCHAR(30))")
+def sqldb(function):
+    def wrapper():
+        db = sq.connect(os.environ.get("SQLITECLOUD"))
+        c = db.cursor()
+        c.execute("CREATE TABLE IF NOT EXISTS hi(id INT primary key, name VARCHAR(30))")
+        final = function(c)
+        db.commit()
+        db.close()
+        return final
+    return wrapper
 
 @app.route("/")
-def home():
+@sqldb
+def home(c):
     mylist = []
     for row in c.execute("SELECT * FROM hi"):
         mylist.append(f"ID: {row[0]} Name: {row[1]}")
     return render_template("index.html", mylist=mylist)
 
 @app.route("/addevent", methods=["GET", "POST"])
-def addevent():
+@sqldb
+def addevent(c):
     if request.method == "POST":
         event_id = request.form.get("eventid")
         event_name = request.form.get("eventname")
         if event_id and event_name:
             c.execute("INSERT INTO hi(id, name) VALUES (?, ?)", (event_id, event_name))
-            db.commit()
             return redirect(url_for("home"))
         return "Missing data or Wrong ID"
     return render_template("addevent.html")
 
 @app.route("/deleteevent", methods=["GET", "POST"])
-def deleteevent():
+@sqldb
+def deleteevent(c):
     if request.method == "POST":
         event_id = request.form.get("eventid")
         try:
             c.execute("DELETE FROM hi WHERE id=(?)", event_id)
-            db.commit()
             return redirect(url_for("home"))
         except Exception as e:
             return f"Error: {e}"
