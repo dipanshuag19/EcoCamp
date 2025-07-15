d323e33be1edea4b7d22162bde02c3d4aca8d754
Dipanshu Ashok Agarwal <hu1243009@sjchs.edu.in>
2025-07-15 18:41:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, render_template, jsonify
+from flask import Flask, request, redirect, url_for, render_template, jsonify, render_template_engine
 #import sqlite3 as sq
 import os
 import sqlitecloud as sq
@@ -25,6 +25,26 @@ def addevent():
         if event_id and event_name:
             c.execute("INSERT INTO hi(id, name) VALUES (?, ?)", (event_id, event_name))
             db.commit()
-            return redirect(url_for("get_message"))
+            return redirect(url_for("home"))
         return "Missing data or Wrong ID"
     return render_template("addevent.html")
+
+@app.route("/deleteevent", methods=["GET", "POST"])
+def deleteevent():
+    if request.method == "POST":
+        event_id = request.form.get("eventid")
+        try:
+            c.execute("DELETE FROM hi WHERE id=(?)", event_id))
+            db.commit()
+            return redirect(url_for("home")) 
+        except Exception as e:
+            return f"Error: {e}"
+    html_code = """<html>
+    <head><title>Delete event</title></head>
+    <body>
+    <form onsubmit='/deleteevent' method='POST'>
+    Enter event id to delete: <input type='number' name='eventid'><br><br>
+    <button type='submit'></button></body></html>
+    """
+    return render_template_engine(html_code)
+        
