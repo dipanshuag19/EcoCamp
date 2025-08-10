c02adb6395028e2baa5f1fd530a7a94cbd167871
Dipanshu Ashok Agarwal <hu1243009@sjchs.edu.in>
2025-08-10 18:49:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,15 @@ def home2(c):
     # eid,ename,email,desc,stime,etime,edate,location,category in edetailslist
     c.execute("SELECT * FROM eventdetail")
     edetailslist = c.fetchall()
-    return render_template("index2.html", edetailslist=edetailslist)
+    treeplant, blooddonate, cleandrive = [], [], []
+    for x in edetailslist:
+        if x['category'] == 'Tree Plantation':
+            treeplant.append(x)
+        elif x['category'] == 'Blood Donation':
+            blooddonate.append(x):
+        else:
+            cleandrive.append(x)
+    return render_template("index2.html", edetailslist=edetailslist, treeplantation=treeplant, blooddonation=blooddonate, cleanlinesdrive=cleandrive)
 
 @app.route("/addevent", methods=["GET", "POST"])
 @sqldb
